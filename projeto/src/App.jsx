df63bf026f60e48f675a7eff5c3f379d7f295e28
lucasdeabreufonseca <lucasdeabreufonseca@gmail.com>
2024-03-19 16:19:13 -0300

    asdasd

---
+++ projeto/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+    <h2>sadfafdasd</h2>
+      <h1>hello world</h1>
+    </>
+  )
+}
+
+export default home
---
+++ projeto/src/Home.jsx
@@ -0,0 +1,9 @@
+export const home = () => {
+    return(
+    <>
+        <h2>sadfafdasd</h2>
+      <h1>hello world</h1>
+    </>
+    )
+   
+}
